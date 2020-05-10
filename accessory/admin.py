d4d1ab99216a9aaf8aa56a32e085fa66d289a634
ijatim <hamed.k_99@yahoo.com>
2020-05-11 00:46:19 +0430

    Alpha version (Accessories Added)

--- accessory/admin.py
+++ accessory/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import (BackCover,
+                     Glass,
+                     HandsFree,
+                     AUXJack,
+                     MobileCable,
+                     MobileCharger,
+                     CarCharger,
+                     PowerBank,
+                     WirelessCharger)
 
-# Register your models here.
+admin.site.register(BackCover)
+admin.site.register(Glass)
+admin.site.register(HandsFree)
+admin.site.register(AUXJack)
+admin.site.register(MobileCharger)
+admin.site.register(MobileCable)
+admin.site.register(CarCharger)
+admin.site.register(PowerBank)
+admin.site.register(WirelessCharger)
--- accessory/models.py
+++ accessory/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from mobile.models import Model, Color
 
-# Create your models here.
+
+class BackCover(models.Model):
+    model = models.ForeignKey(Model, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    price = models.CharField('Price (1000T)', max_length=5, validators=[RegexValidator(r'^\d{1,10}$')])
+
+
+class Glass(models.Model):
+    model = models.ForeignKey(Model, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    price = models.CharField('Price (1000T)', max_length=5, validators=[RegexValidator(r'^\d{1,10}$')])
+
+
+class HandsFree(models.Model):
+    handsfree_types = [('J Series', 'J Series'),]
+    type = models.CharField(max_length=10, choices=handsfree_types)
+    image = models.ImageField(null=True, blank=True)
+    price = models.CharField('Price (1000T)', max_length=5, validators=[RegexValidator(r'^\d{1,10}$')])
+
+
+class AUXJack(models.Model):
+    pass
+
+
+class MobileCable(models.Model):
+    pass
+
+
+class MobileCharger(models.Model):
+    pass
+
+
+class CarCharger(models.Model):
+    pass
+
+
+class PowerBank(models.Model):
+    pass
+
+
+class WirelessCharger(models.Model):
+    pass
